2746af879364721c5357b441f5a81409893f5dad
A'laa Magdy <124933097+Aalaa-magdy@users.noreply.github.com>
2026-03-06 14:11:40 +0200

    Time: 59 ms (37.46%) | Memory: 54.6 MB (33.22%) - LeetSync

---
+++ 2821-timeout-cancellation/timeout-cancellation.js
@@ -0,0 +1,11 @@
+var cancellable = function(fn, args, t) {
+
+    const timer = setTimeout(() => {
+        fn(...args);
+    }, t);
+
+    return function cancelFn() {
+        clearTimeout(timer);
+    };
+
+};
